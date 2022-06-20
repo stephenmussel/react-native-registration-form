9ac757080a9361d3bf6c7e8600a5cbf423788455
phaydara <phaydara@gmail.com>
2022-06-19 22:11:00 -0500

    installed Formik dependencies

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import React, { useState } from 'react';
+import { Formik } from 'formik';
 
 // const styles = StyleSheet.create({
 //   container: {
